9282a1b0e82e6dc597cbb09357098226456ec21e
cho5butter <akutore@gmail.com>
2021-01-29 01:43:21 +0900

    second commit

--- MagPrime/MainWindow.xaml.cs
+++ MagPrime/MainWindow.xaml.cs
@@ -29,33 +29,79 @@ namespace MagPrime
         {
             public ImageSource imageSource { get; set; }
             public string appName { get; set; }
+            public Process appProcess { get; set; }
         }
+        private struct RECT
+        {
+            //型はlongじゃなくてintが正解！！！！！！！！！！！！！！
+            //longだとおかしな値になる
+            public int left;
+            public int top;
+            public int right;
+            public int bottom;
+        }
+        private ObservableCollection<Application> _apps;
         public MainWindow()
         {
             InitializeComponent();
 
-            ObservableCollection<Application> apps = new ObservableCollection<Application>();
+            this._apps = new ObservableCollection<Application>();
             foreach (Process p in Process.GetProcesses())
             {
                 if (p.MainWindowTitle.Length != 0)
                 {
-                    //try
-                    //{
+                    try
+                    {
                         Console.WriteLine(p.MainModule.FileName);
                         System.Drawing.Icon ico = System.Drawing.Icon.ExtractAssociatedIcon(p.MainModule.FileName);
                         Bitmap bitmap = ico.ToBitmap();
                         IntPtr hBitmap = bitmap.GetHbitmap();
                         ImageSource wpfBitmap = Imaging.CreateBitmapSourceFromHBitmap(hBitmap, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
-                        apps.Add(new Application() { imageSource = wpfBitmap, appName = p.MainWindowTitle });
+                        this._apps.Add(new Application() {
+                            imageSource = wpfBitmap,
+                            appName = p.MainWindowTitle,
+                            appProcess = p
+                        });
 
-                    //}
-                    //catch(System.ComponentModel.Win32Exception)
-                    //{
-                    //    Console.WriteLine(p.MainWindowTitle);
-                    //}
+                    }
+                    catch(System.ComponentModel.Win32Exception)
+                    {
+                        Console.WriteLine(p.MainWindowTitle);
+                    }
                 }
             }
-            appList.ItemsSource = apps;
+            appList.ItemsSource = _apps;
+        }
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int x, int y, int cx, int cy, int uFlags);
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+        const int SWP_NOMOVE = 0x0001;
+        const int SWP_NOZORDER = 0x0004;
+        void PrintText(object sender, SelectionChangedEventArgs args)
+        {
+            if (appList.SelectedIndex == -1) return;
+            Process p = this._apps[appList.SelectedIndex].appProcess;
+            p.WaitForInputIdle();
+            //MoveWindow(p.MainWindowHandle, 100, 100, 300, 300, 1);
+            int windowWidth = 500;
+            int windowHeight = 500;
+            if(GetWindowRect(p.MainWindowHandle, out RECT wRect))
+            {
+                windowWidth = wRect.right - wRect.left;
+                windowHeight = wRect.bottom - wRect.top;
+            }
+            SetWindowPos(
+                p.MainWindowHandle,
+                0,
+                (System.Windows.Forms.Screen.PrimaryScreen.WorkingArea.Width - windowWidth)/2,
+                (System.Windows.Forms.Screen.PrimaryScreen.WorkingArea.Height - windowHeight) / 2,
+                300,
+                300,
+                SWP_NOMOVE | SWP_NOZORDER
+                );
+            appList.SelectedIndex = -1;
+            this.Close();
         }
     }
 }
